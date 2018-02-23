2524de1b18d555593ecab280ab3c64cae0c56448
KDercksen <mail@koendercksen.com>
2018-02-23 15:09:59 +0100

    Utility functions to read images, load labels (see #2)

--- util.py
+++ util.py
@@ -3,18 +3,58 @@
 
 from constants import DATA_DIR
 from keras.preprocessing.image import img_to_array, load_img
-from os.path import join
 from pandas import read_csv
+import os
+import os.path as osp
 
 
 def read_img(img_id, train_or_test, size):
+    '''Read a single image into NumPy array.
+
+    Arguments:
+    ----------
+    img_id: string
+        Image ID. For example if image filename is xxx.jpg, 'xxx' is the ID.
+    train_or_test: string
+        'train' or 'test'.
+    size: int
+        Size of image (size x size), for example 224 for VGG16 network or 299 for XCeption network.
+
+    Returns:
+    --------
+    img_array: numpy array
+        Image loaded as NumPy array.
+    '''
     if train_or_test not in ['train', 'test']:
         raise ValueError('wrong value for train_or_test!')
-    path = join(DATA_DIR, train_or_test, f'{img_id}.jpg')
+    path = osp.join(DATA_DIR, train_or_test, f'{img_id}.jpg')
     img = load_img(path, target_size=size)
     return img_to_array(img)
 
 
 def get_labels():
-    path = join(DATA_DIR, 'labels.csv')
+    '''Get training labels.
+
+    Returns:
+    --------
+    labels: pandas dataframe
+        Dataframe has two columns, 'id' and 'breed'. 'id' is the image id,
+        'breed' is the classlabel for that image.
+    '''
+    path = osp.join(DATA_DIR, 'labels.csv')
     return read_csv(path)
+
+
+def get_images(train_or_test, size):
+    '''Generator that yields images from train or test set.
+
+    Arguments:
+    ----------
+    train_or_test: string
+        'train' or 'test'.
+    size: int
+        Size of image (size x size), for example 224 for VGG16 network or 299 for XCeption network.
+    '''
+    for img in os.listdir(osp.join(DATA_DIR, train_or_test)):
+        img_id = osp.splitext(osp.basename(img))[0]
+        yield read_img(img_id, train_or_test, size)
