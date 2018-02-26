35d6642081120fe47d47351eb919609ac7cef92a
KDercksen <mail@koendercksen.com>
2018-02-26 12:23:26 +0100

    Clean up code in main.py, fix a small mistake in util.py (#2)

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-import numpy as np
-import pandas as pd
-from tqdm import tqdm
-from sklearn.metrics import log_loss
-from os.path import join
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from constants import NUM_CLASSES, SEED
 from keras.applications import inception_v3
-from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D
-from util import *
-from constants import *
+from keras.models import Model
+from sklearn.metrics import log_loss
+from tqdm import tqdm
+from util import get_labels, get_images
+import numpy as np
 
 '''Define constants'''
 np.random.seed(seed=SEED)
 INPUT_SIZE = 299
 
 '''Load labels'''
-labels = pd.read_csv(join(DATA_DIR, 'labels.csv'))
+print('Load labels...')
+labels = get_labels()
 labels['target'] = 1
 labels['rank'] = labels.groupby('breed').rank()['id']
-labels_pivot = labels.pivot('id', 'breed', 'target').reset_index().fillna(0).drop(columns=['id'])
+labels_pivot = labels.pivot('id', 'breed', 'target').reset_index().fillna(0) \
+                                                    .drop(columns=['id'])
 
 '''Load training data'''
+print('Load training data...')
 images = np.zeros((len(labels), INPUT_SIZE, INPUT_SIZE, 3), dtype='float16')
-for i, img_id in tqdm(enumerate(labels['id'])):
-    img = read_img(img_id, 'train', (INPUT_SIZE, INPUT_SIZE))
+for i, img in tqdm(enumerate(get_images('train', INPUT_SIZE))):
     x = inception_v3.preprocess_input(np.expand_dims(img.copy(), axis=0))
     images[i] = x
 
 '''Split into training (~80%) and validation set (~20%)'''
+print('Create train/val split...')
 rnd = np.random.random(len(labels))
 train_idx = rnd < 0.8
 valid_idx = rnd >= 0.8
@@ -41,6 +45,7 @@ x_valid = images[valid_idx]
     Add a single fully connected layer on top of the conv layers of Inception
     Freeze Inception layers
 '''
+print('Define and fit model...')
 base_model = inception_v3.InceptionV3(weights='imagenet', include_top=False)
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
@@ -51,17 +56,22 @@ model = Model(inputs=base_model.input, outputs=y)
 for layer in base_model.layers:
     layer.trainable = False
 
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
 '''Fit model on data'''
 model.fit(x_train, y_train, batch_size=32, epochs=20, verbose=1)
 
 '''Save model'''
-model.save('model1.h5')
+fname = 'model1.h5'
+print(f'Saving model to {fname}')
+model.save(fname)
 
 '''Predict validation set'''
+print('Predict val set...')
 pred_valid = model.predict(x_valid, batch_size=32, verbose=1)
 
 '''Evaluate predictions'''
-print('Validation log loss {}'.format(log_loss(y_valid, pred_valid)))
-#TODO: accuracy on validation set
+print(f'Validation log loss {log_loss(y_valid, pred_valid)}')
+
+# TODO: accuracy on validation set
--- util.py
+++ util.py
@@ -29,7 +29,7 @@ def read_img(img_id, train_or_test, size):
     if train_or_test not in ['train', 'test']:
         raise ValueError('wrong value for train_or_test!')
     path = osp.join(DATA_DIR, train_or_test, f'{img_id}.jpg')
-    img = load_img(path, target_size=size)
+    img = load_img(path, target_size=(size, size))
     return img_to_array(img)
 
 
